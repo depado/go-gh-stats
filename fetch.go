8ee4d4370da1531f93cdd99c0981261f25fd5602
Depado <paul.lhussiez@gmail.com>
2015-07-19 01:37:54 +0200

    Hm. Stuff ?

--- fetch.go
+++ fetch.go
@@ -7,13 +7,14 @@ import (
 
 // Fetch the url given in parameter and unmarshall the JSON to the given out struct
 func fetchURL(url string, out interface{}) (err error) {
-	client := new(http.Client)
+	c := new(http.Client)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return
 	}
+	// Add the API version in Headers
 	req.Header.Add("Accept", "application/vnd.github.v3+json")
-	resp, err := client.Do(req)
+	resp, err := c.Do(req)
 	defer resp.Body.Close()
 	dec := json.NewDecoder(resp.Body)
 	err = dec.Decode(out)
